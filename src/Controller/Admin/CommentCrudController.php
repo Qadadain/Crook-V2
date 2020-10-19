ab7376c27505af20578bc991418e2c5d135f23aa
Rolls47 <quentin.adadain@gmail.com>
2020-10-19 16:16:01 +0200

    CSS nav profil and easyadmin comment crud fix

--- src/Controller/Admin/CommentCrudController.php
+++ src/Controller/Admin/CommentCrudController.php
@@ -21,9 +21,9 @@ class CommentCrudController extends AbstractCrudController
     public function configureFields(string $pageName): iterable
     {
         return [
-            AssociationField::new('user', 'Pseudo'),
-            AssociationField::new('tutorial', 'Tutoriel'),
-            IdField::new('id')->hideOnIndex(),
+            IdField::new('id')->hideOnIndex()->hideOnForm(),
+            AssociationField::new('user', 'Pseudo')->hideOnForm(),
+            AssociationField::new('tutorial', 'Tutoriel')->hideOnForm(),
             TextField::new('comment', 'Commentaire'),
             DateField::new('createAt', 'Crée le')->hideOnForm(),
             DateField::new('updateAt', 'Maj')->hideOnForm(),
